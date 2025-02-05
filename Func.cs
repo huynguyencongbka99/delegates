2c858038e367479ebd723aa29c76a6221f9cc10d
huynguyencongbka99 <69630827+huynguyencongbka99@users.noreply.github.com>
2025-02-05 10:50:47 +0700

    Update Func.cs
    Một cách thức khởi tạo và trả về nhanh một asynchronous function

--- Func.cs
+++ Func.cs
@@ -1,3 +1,6 @@
+// Đây là cách khá thú vị để có thể sử dụng một function chạy background thread
+// Chúng ta sẽ dùng nó để có thể update và trả về kết quả ngay lập tức trên Main(UI) thread mà không cần khai báo trước function
+// Hàm phía dưới có thể lấy ngay một biến đầu vào là x = 10, thực thi câu lệnh và trả về kết quả ngay trong một function nào đó.
 int mm = 42;
 int result = (int)this.Invoke(new Func<int, int>((x) =>
 {
